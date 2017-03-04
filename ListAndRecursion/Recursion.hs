b762ef03eb84468131cee9ccf77dc65dd1dfbacd
snowtsuku <snowtsuku@live.com>
2017-03-04 22:39:21 +0800

    Added dropInt function that drops n elements fron an integer array

--- ListAndRecursion/Recursion.hs
+++ ListAndRecursion/Recursion.hs
@@ -3,3 +3,9 @@ takeInt :: Int -> [Int] -> [Int]
 takeInt _ [] = []
 takeInt 0 _ = []
 takeInt n (x:xs) = x:(takeInt (n-1) xs)
+
+--Removes n number of elements from an integer array
+dropInt :: Int -> [Int] -> [Int]
+dropInt _ [] = []
+dropInt 0 list = list
+dropInt n (x:xs) = dropInt (n-1) xs
