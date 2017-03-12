611b919f7d7fc95bbc02d663f410e106d3cae8b5
snowtsuku <snowtsuku@live.com>
2017-03-12 11:38:36 +0800

    Added fs_maximum that gets the maximum value from the list

--- ListAndRecursion/FoldAndScan.hs
+++ ListAndRecursion/FoldAndScan.hs
@@ -6,3 +6,10 @@ and list = foldr (&&) True list
 or :: [Bool] -> Bool
 or [] = error "or: empty list"
 or list = foldr (||) False list
+
+fs_maximum [] = error "fs_maximum: empty list"
+fs_maximum list = foldr1 higher list
+    where
+    higher a b
+        | a > b = a
+        | otherwise = b
